8d09ba83da2ac48a3b660c2af55445c07b5f31d1
Cristian Pop <cristian.pop@analog.com>
2021-11-15 16:12:36 +0200

    Update with pull request.
    Needs to be prepared for upstream.
    
    Signed-off-by: Cristian Pop <cristian.pop@analog.com>

--- drivers/iio/frequency/admv4420.c
+++ drivers/iio/frequency/admv4420.c
@@ -1,23 +1,15 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: GPL-2.0 OR BSD-2-Clause
 /*
  * ADMV4420
  *
  * Copyright 2021 Analog Devices Inc.
  */
 
-#include <linux/bitfield.h>
-#include <linux/module.h>
-#include <linux/regmap.h>
-#include <linux/kernel.h>
-#include <linux/firmware.h>
-#include <linux/of_device.h>
-#include <linux/spi/spi.h>
-#include <linux/err.h>
-#include <linux/debugfs.h>
-
-#include <linux/of.h>
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
+#include <linux/module.h>
+#include <linux/regmap.h>
+#include <linux/spi/spi.h>
 
 /* ADMV4420 Register Map */
 #define ADMV4420_SPI_CONFIG_1			0x00
@@ -58,6 +50,9 @@
 #define ADMV4420_CP_CURRENT			0x22E
 #define ADMV4420_CP_BLEED			0x22F
 
+#define ADMV4420_SPI_CONFIG_1_SOFTRESET		BIT(1)
+#define ADMV4420_SPI_CONFIG_1_SOFTRESET_	BIT(7)
+
 #define ADMV4420_REFERENCE_IN_MODE(x)		(x << 1)
 #define ADMV4420_REFERENCE_DOUBLER(x)		(x << 2)
 #define ADMV4420_REFERENCE_DIVIDE_BY_2_MASK	BIT(0)
@@ -75,6 +70,9 @@
 #define ENABLE_IFAMP				BIT(2)
 #define ENABLE_MIXER				BIT(1)
 #define ENABLE_LNA				BIT(0)
+
+#define ADAR1000_SCRATCH_PAD_VAL_1	0xAD
+#define ADAR1000_SCRATCH_PAD_VAL_2	0xEA
 
 enum admv4420_option_st {
 	ADMV4420_DISABLED,
@@ -123,12 +121,13 @@ struct admv4420_n_counter {
 struct admv4420_state {
 	struct spi_device		*spi;
 	struct regmap			*regmap;
-	u64		 		pfd_freq_hz;
-	u64 				vco_freq_hz;
-	u64 				lo_freq_hz;
+	u64				pfd_freq_hz;
+	u64				vco_freq_hz;
+	u64				lo_freq_hz;
 	struct admv4420_reference_block ref_block;
 	struct admv4420_n_counter	n_counter;
 	enum admv4420_mux_sel		mux_sel;
+	struct mutex			lock;
 };
 
 static const char *const admv4420_op_state[] = {
@@ -167,18 +166,16 @@ static int admv4420_reg_access(struct iio_dev *indio_dev,
 		return regmap_write(st->regmap, reg, writeval);
 }
 
-static int admv4420_calc_vco_freq(struct admv4420_state *st)
+static void admv4420_calc_vco_freq(struct admv4420_state *st)
 {
 	u64 tmp;
 
 	tmp = div_u64((st->pfd_freq_hz * st->n_counter.frac_val), st->n_counter.mod_val);
 	tmp += st->pfd_freq_hz * st->n_counter.int_val;
 	st->vco_freq_hz = tmp;
-
-	return 0;
 }
 
-static int admv4420_calc_pfd_freq(struct admv4420_state *st)
+static void admv4420_calc_pfd_freq(struct admv4420_state *st)
 {
 	u32 tmp;
 
@@ -189,8 +186,6 @@ static int admv4420_calc_pfd_freq(struct admv4420_state *st)
 
 	admv4420_calc_vco_freq(st);
 	st->lo_freq_hz = st->vco_freq_hz * 2;
-
-	return 0;
 }
 
 static int admv4420_set_n_counter(struct admv4420_state *st, u32 int_val, u32 frac_val, u32 mod_val)
@@ -229,8 +224,8 @@ static int admv4420_set_n_counter(struct admv4420_state *st, u32 int_val, u32 fr
 }
 
 static ssize_t admv4420_mux_sel_show_available(struct device *dev,
-					   struct device_attribute *attr,
-					   char *buf)
+					       struct device_attribute *attr,
+					       char *buf)
 {
 	size_t len = 0;
 	int i;
@@ -249,7 +244,7 @@ static ssize_t admv4420_mux_sel_show(struct device *dev,
 {
 	struct iio_dev *indio_dev = dev_to_iio_dev(dev);
 	struct admv4420_state *st = iio_priv(indio_dev);
-	
+
 	return sprintf(buf, "%s\n", admv4420_mux_sel_str[st->mux_sel]);
 }
 
@@ -282,7 +277,7 @@ static ssize_t admv4420_ref_type_show(struct device *dev,
 {
 	struct iio_dev *indio_dev = dev_to_iio_dev(dev);
 	struct admv4420_state *st = iio_priv(indio_dev);
-	
+
 	return sprintf(buf, "%s\n", admv4420_ref_type_str[st->ref_block.type]);
 }
 
@@ -293,7 +288,8 @@ static ssize_t admv4420_ref_doubler_show(struct device *dev,
 	struct iio_dev *indio_dev = dev_to_iio_dev(dev);
 	struct iio_dev_attr *this_attr = to_iio_dev_attr(attr);
 	struct admv4420_state *st = iio_priv(indio_dev);
-	switch(this_attr->address) {
+
+	switch (this_attr->address) {
 	case ADMV4420_DOUBLER:
 		return sprintf(buf, "%s\n", admv4420_op_state[st->ref_block.doubler_en]);
 	case ADMV4420_DIVIDE_BY_2:
@@ -311,7 +307,7 @@ static ssize_t admv4420_n_counter_show(struct device *dev,
 	struct iio_dev *indio_dev = dev_to_iio_dev(dev);
 	struct admv4420_state *st = iio_priv(indio_dev);
 
-	switch(this_attr->address) {
+	switch (this_attr->address) {
 	case ADMV4420_N_COUNTER_INT:
 		return sprintf(buf, "%d\n", st->n_counter.int_val);
 	case ADMV4420_N_COUNTER_FRAC:
@@ -333,11 +329,12 @@ static ssize_t admv4420_n_counter_store(struct device *dev,
 	u32 readval;
 	int ret = 0;
 
+	mutex_lock(&st->lock);
 	ret = kstrtou32(buf, 10, &readval);
 	if (ret)
 		return ret;
 
-	switch(this_attr->address) {
+	switch (this_attr->address) {
 	case ADMV4420_N_COUNTER_INT:
 		st->n_counter.int_val = readval;
 		break;
@@ -354,12 +351,15 @@ static ssize_t admv4420_n_counter_store(struct device *dev,
 	ret = admv4420_set_n_counter(st, st->n_counter.int_val, st->n_counter.frac_val,
 				     st->n_counter.mod_val);
 	if (ret < 0)
-		return ret;
+		goto err_unlock;
 
 	admv4420_calc_vco_freq(st);
 	st->lo_freq_hz = st->vco_freq_hz * 2;
 
-	return len;
+err_unlock:
+	mutex_unlock(&st->lock);
+
+	return ret ? ret : len;
 }
 
 static ssize_t admv4420_ref_divider_show(struct device *dev,
@@ -378,7 +378,7 @@ static ssize_t admv4420_ref_freq_show(struct device *dev,
 {
 	struct iio_dev *indio_dev = dev_to_iio_dev(dev);
 	struct admv4420_state *st = iio_priv(indio_dev);
-	
+
 	return sprintf(buf, "%d\n", st->ref_block.freq_hz);
 }
 
@@ -481,11 +481,9 @@ static int admv4420_dt_parse(struct admv4420_state *st)
 	st->ref_block.divide_by_2_en = of_property_read_bool(spi->dev.of_node,
 							     "adi,ref_divide_by_2_en");
 	of_property_read_u32(spi->dev.of_node, "adi,ref_divider", &st->ref_block.divider);
-	
 	of_property_read_u32(spi->dev.of_node, "adi,N_counter_int_val", &st->n_counter.int_val);
 	of_property_read_u32(spi->dev.of_node, "adi,N_counter_frac_val", &st->n_counter.frac_val);
 	of_property_read_u32(spi->dev.of_node, "adi,N_counter_mod_val", &st->n_counter.mod_val);
-
 	of_property_read_u32(spi->dev.of_node, "adi,mux_sel", &st->mux_sel);
 
 	return 0;
@@ -496,42 +494,38 @@ static int admv4420_setup(struct iio_dev *indio_dev)
 	struct admv4420_state *st = iio_priv(indio_dev);
 	u32 val = 0;
 	int ret;
-	pr_err("%s: %d: Enter ADMV4420 probe\n", __func__, __LINE__);
+
 	/* Software reset and activate SDO */
-	ret = regmap_write(st->regmap, ADMV4420_SPI_CONFIG_1, 0x81);
+	ret = regmap_write(st->regmap, ADMV4420_SPI_CONFIG_1,
+			   ADMV4420_SPI_CONFIG_1_SOFTRESET_ | ADMV4420_SPI_CONFIG_1_SOFTRESET);
 	if (ret < 0)
 		return ret;
 
-	ret = regmap_write(st->regmap, ADMV4420_SCRATCHPAD, 0xAD);
+	ret = regmap_write(st->regmap, ADMV4420_SCRATCHPAD, ADAR1000_SCRATCH_PAD_VAL_1);
 	if (ret < 0)
 		return ret;
-pr_err("%s: %d: Enter ADMV4420 probe\n", __func__, __LINE__);
+
 	ret = regmap_read(st->regmap, ADMV4420_SCRATCHPAD, &val);
 	if (ret < 0)
 		return ret;
 
-pr_err("%s: %d: Enter ADMV4420 probe\n", __func__, __LINE__);
-
-	if (val != 0xAD) {
+	if (val != ADAR1000_SCRATCH_PAD_VAL_1) {
 		dev_err(indio_dev->dev.parent, "Failed ADMV4420 to read/write scratchpad %x ", val);
 		return -EIO;
 	}
 
-	ret = regmap_write(st->regmap, ADMV4420_SCRATCHPAD, 0x5A);
+	ret = regmap_write(st->regmap, ADMV4420_SCRATCHPAD, ADAR1000_SCRATCH_PAD_VAL_2);
 	if (ret < 0)
 		return ret;
-pr_err("%s: %d: Enter ADMV4420 probe\n", __func__, __LINE__);
+
 	ret = regmap_read(st->regmap, ADMV4420_SCRATCHPAD, &val);
 	if (ret < 0)
 		return ret;
 
-pr_err("%s: %d: Enter ADMV4420 probe\n", __func__, __LINE__);
-
-	if (val != 0x5A) {
+	if (val != ADAR1000_SCRATCH_PAD_VAL_2) {
 		dev_err(indio_dev->dev.parent, "Failed ADMV4420 to read/write scratchpad %x ", val);
 		return -EIO;
 	}
-	dev_err(indio_dev->dev.parent, "ADMV4420 to read/write scratchpad %x ", val);
 
 	st->ref_block.freq_hz = 50000000;
 	st->ref_block.type = ADMV4420_XTAL;
@@ -547,7 +541,6 @@ pr_err("%s: %d: Enter ADMV4420 probe\n", __func__, __LINE__);
 
 	admv4420_dt_parse(st);
 
-	pr_err("%s: %d: admv4420_setup %x\n", __func__, __LINE__, st->ref_block.divider);
 	ret = regmap_write(st->regmap, ADMV4420_R_DIV_L,
 			   FIELD_GET(0xFF, st->ref_block.divider));
 	if (ret < 0)
@@ -557,11 +550,7 @@ pr_err("%s: %d: Enter ADMV4420 probe\n", __func__, __LINE__);
 			   FIELD_GET(0xFF00, st->ref_block.divider));
 	if (ret < 0)
 		return ret;
-	pr_err("%s: %d: admv4420_setup %x %x %x %x\n", __func__, __LINE__, st->ref_block.divide_by_2_en, ADMV4420_REFERENCE_IN_MODE(st->ref_block.type), ADMV4420_REFERENCE_DOUBLER(st->ref_block.doubler_en),
-		st->ref_block.divide_by_2_en |
-			   ADMV4420_REFERENCE_IN_MODE(st->ref_block.type) |
-			   ADMV4420_REFERENCE_DOUBLER(st->ref_block.doubler_en)
-	);
+
 	ret = regmap_write(st->regmap, ADMV4420_REFERENCE,
 			   st->ref_block.divide_by_2_en |
 			   ADMV4420_REFERENCE_IN_MODE(st->ref_block.type) |
@@ -593,34 +582,33 @@ static int admv4420_probe(struct spi_device *spi)
 	struct admv4420_state *st;
 	struct regmap *regmap;
 	int ret;
-pr_err("%s: %d: Enter ADMV4420 probe\n", __func__, __LINE__);
+
 	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
 	if (!indio_dev)
 		return -ENOMEM;
-pr_err("%s: %d: Enter ADMV4420 probe\n", __func__, __LINE__);	
+
 	regmap = devm_regmap_init_spi(spi, &admv4420_regmap_config);
 	if (IS_ERR(regmap)) {
 		dev_err(&spi->dev, "Error  ADMV4420 initializing spi regmap: %ld\n",
 			PTR_ERR(regmap));
 		return PTR_ERR(regmap);
 	}
-	
+
 	st = iio_priv(indio_dev);
 	st->spi = spi;
 	st->regmap = regmap;
-	
-pr_err("%s: %d: Enter ADMV4420 probe\n", __func__, __LINE__);
+	mutex_init(&st->lock);
+
 	indio_dev->dev.parent = &spi->dev;
 	indio_dev->name = "admv4420";
 	indio_dev->info = &admv4420_info;
-pr_err("%s: %d: Enter ADMV4420 probe\n", __func__, __LINE__);
-	ret = admv4420_setup(indio_dev);
 
+	ret = admv4420_setup(indio_dev);
 	if (ret < 0) {
 		dev_err(&spi->dev, "Setup ADMV4420 failed (%d)\n", ret);
 		return ret;
 	}
-	pr_err("%s: %d: Enter ADMV4420 probe\n", __func__, __LINE__);
+
 	return devm_iio_device_register(&spi->dev, indio_dev);
 }
 
@@ -628,6 +616,7 @@ static const struct of_device_id admv4420_of_match[] = {
 	{ .compatible = "adi,admv4420" },
 	{ },
 };
+
 MODULE_DEVICE_TABLE(of, admv4420_of_match);
 
 static struct spi_driver admv4420_driver = {
@@ -641,4 +630,4 @@ module_spi_driver(admv4420_driver);
 
 MODULE_AUTHOR("Cristian Pop <cristian.pop@analog.com>");
 MODULE_DESCRIPTION("Analog Devices ADMV44200 K Band Downconverter");
-MODULE_LICENSE("GPL v2");
+MODULE_LICENSE("Dual BSD/GPL");
